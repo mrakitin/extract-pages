b8accdc401623ed5ddd11f8797ec7495b2bbbdd3
Maksim Rakitin <mrakitin@bnl.gov>
2020-02-11 00:28:05 -0500

    Add a test

--- extract_pages/tests/test_examples.py
+++ extract_pages/tests/test_examples.py
@@ -1,3 +1,6 @@
-def test_one_plus_one_is_two():
-    "Check that one and one are indeed two."
-    assert 1 + 1 == 2
+from extract_pages import parse_ranges
+
+
+def test_extract_pages():
+    res = parse_ranges('1,3-6')
+    assert res == [1, 3, 4, 5, 6]
